6fa6629dc79e797d3f44dc17290a186104678001
Luke Mainwaring <lukemainwaring@Lukes-MacBook-Pro-3.local>
2020-11-10 22:40:34 -0600

    add nivo bar chart w test data

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,16 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Typography, withStyles } from '@material-ui/core';
 
-const Home = ({ }) => {
+import MyResponsiveBar from './MyResponsiveBar';
+import { testBarData } from '../util/testData';
 
+const Home = ({}) => {
   return (
     <div>
       <Typography variant='h2' component='h2' align='center'>
         Beatport Analysis
       </Typography>
+      <MyResponsiveBar data={testBarData} title={'My Bar Chart 1'} />
     </div>
   );
 };
